60cc0a6acfde62ee691576afc3debeff058fe6f6
Jordan-Shaw <jordan.shaw2400@gmail.com>
2019-12-09 11:38:00 +0000

    Added create_table_comments_table migration file

---
+++ db/migrations/20191209113109_create_table_comments_table.js
@@ -0,0 +1,17 @@
+
+exports.up = function (knex) {
+  console.log('Creating comments table...');
+  return knex.schema.createTable('comments', (commentsTable) => {
+    commentsTable.increments('comment_id').primary();
+    commentsTable.string('author').references('users.username');
+    commentsTable.integer('article_id').references('articles.article_id');
+    commentsTable.integer('votes').defaultTo(0);
+    commentsTable.timestamp('created_at', { precision: 6 }).defaultTo(knex.fn.now(6));
+    commentsTable.string('body', 750);
+  })
+};
+
+exports.down = function (knex) {
+  console.log('Removing comments table...');
+  return knex.dropTable('comments');
+};
