6676801a92a5ece9fb4f7665b2195c2e4bb5591c
Jordan-Shaw <jordan.shaw2400@gmail.com>
2019-12-09 11:09:37 +0000

    migration create_table_users_tabloe added

---
+++ db/migrations/20191209110350_create_table_users_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function (knex) {
+  console.log('Creating users table...');
+  return knex.schema.createTable('users', (usersTable) => {
+    usersTable.string('username').primary();
+    usersTable.string('avatar_url');
+    usersTable.string('name');
+  })
+};
+
+exports.down = function (knex) {
+  console.log('Removing users table...');
+  return knex.dropTable('users');
+};
