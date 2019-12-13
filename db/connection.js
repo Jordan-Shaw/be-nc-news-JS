9a0dc860b262bc5130e8dc16200f0bd7e598c60d
Jordan-Shaw <jordan.shaw2400@gmail.com>
2019-12-13 10:13:52 +0000

    altered config to include production database connection, added scripts to package.json, added a listen file and a start script

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,10 @@
+const ENV = process.env.NODE_ENV || 'development'
 const knex = require('knex');
-const config = require('../knexfile.js');
+
+const config =
+  ENV === 'production'
+    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    : require('../knexfile.js');
 
 const knextion = knex(config);
 
