cb843cf0827e3eb33c54f9a032a25fa29cd10594
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-01-08 09:37:12 +0000

    Removed \n's and \t's from spec

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -37,7 +37,7 @@ describe('app', () => {
             expect(user.name).to.equal('jonny');
           })
       });
-      it('/:username GET:404 Returns \'User does not exist\' when passed\n\t a valid username not found in the database', () => {
+      it('/:username GET:404 Returns \'User does not exist\' when passed a valid username not found in the database', () => {
         return request(app)
           .get('/api/users/Jean_Genet')
           .expect(404)
@@ -45,7 +45,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('User does not exist');
           });
       });
-      it('/:username PUT:405 Returns Method Not \n\t Found when put requested', () => {
+      it('/:username PUT:405 Returns Method Not Found when put requested', () => {
         return request(app)
           .put('/api/users/butter_bridge')
           .send({ user: 'butter_bridge', avatar_url: "www.google.com" })
@@ -66,7 +66,7 @@ describe('app', () => {
             expect(article).to.have.keys('article_id', 'title', 'body', 'votes', 'topic', 'author', 'created_at', 'comment_count')
           })
       });
-      it('/:article_id GET:404 Returns \'Article does not exist\' when \n\t passed a valid article_id not found in the database', () => {
+      it('/:article_id GET:404 Returns \'Article does not exist\' when passed a valid article_id not found in the database', () => {
         return request(app)
           .get('/api/articles/9999')
           .expect(404)
@@ -74,7 +74,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Article does not exist');
           });
       });
-      it('/:article_id GET:400 Returns \'Invalid ID provided\' when passed \n\t an invalid article_id', () => {
+      it('/:article_id GET:400 Returns \'Invalid ID provided\' when passed an invalid article_id', () => {
         return request(app)
           .get('/api/articles/The_Thiefs_Journal')
           .expect(400)
@@ -82,7 +82,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Invalid ID provided');
           });
       });
-      it('/:article_id PATCH:200 Successfully patches specified \n\tarticle when provided data in the correct format', () => {
+      it('/:article_id PATCH:200 Successfully patches specified article when provided data in the correct format', () => {
         return request(app)
           .patch('/api/articles/1')
           .send({ inc_votes: 320 })
@@ -92,7 +92,7 @@ describe('app', () => {
             expect(response.body.article.votes).to.equal(420);
           })
       });
-      it('/:article_id PATCH:200 Returns unchanged article \n\t when passed no inc_votes on request body', () => {
+      it('/:article_id PATCH:200 Returns unchanged article when passed no inc_votes on request body', () => {
         return request(app)
           .patch('/api/articles/1')
           .send({})
@@ -101,7 +101,7 @@ describe('app', () => {
             expect(response.body.article.votes).to.equal(100);
           })
       });
-      it('/:article_id PATCH:400 Returns \'Invalid number of votes \n\t to add\' when passed invalid value for \n\t inc_votes on request body \n\t e.g. {inc_votes: \'cats\'}', () => {
+      it('/:article_id PATCH:400 Returns \'Invalid number of votes to add\' when passed invalid value for inc_votes on request body e.g. {inc_votes: \'cats\'}', () => {
         return request(app)
           .patch('/api/articles/1')
           .send({ inc_votes: "cats" })
@@ -110,7 +110,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Invalid number of votes to add');
           })
       });
-      it('/:article_id PATCH:400 Returns \'Invalid properties \n\tin request\' when passed argument other than inc_votes in \n\t the request body', () => {
+      it('/:article_id PATCH:400 Returns \'Invalid properties in request\' when passed argument other than inc_votes in the request body', () => {
         return request(app)
           .patch('/api/articles/1')
           .send({ name: "Jordan" })
@@ -119,7 +119,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Invalid properties in request')
           });
       });
-      it('/:article_id PATCH:400 Returns \'Invalid properties in \n\t request\' when passed extra arguments other than inc_votes in \n\t the request body', () => {
+      it('/:article_id PATCH:400 Returns \'Invalid properties in request\' when passed extra arguments other than inc_votes in the request body', () => {
         return request(app)
           .patch('/api/articles/1')
           .send({ inc_votes: "320", name: "Jordan" })
@@ -128,7 +128,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Invalid properties in request')
           });
       });
-      it('/:article_id PATCH:404 Returns \'Article does not exist\' \n\t when passed incorrect article_id', () => {
+      it('/:article_id PATCH:404 Returns \'Article does not exist\' when passed incorrect article_id', () => {
         return request(app)
           .patch('/api/articles/9999')
           .send({ inc_votes: 1000 })
@@ -137,7 +137,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Article does not exist');
           });
       });
-      it('/:article_id PATCH:400 Returns \'Invalid ID provided\' when passed \n\t an invalid article_id', () => {
+      it('/:article_id PATCH:400 Returns \'Invalid ID provided\' when passed an invalid article_id', () => {
         return request(app)
           .patch('/api/articles/The_Thiefs_Journal')
           .send({ inc_votes: 1000 })
@@ -146,7 +146,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Invalid ID provided');
           });
       });
-      it('/:article_id/comments GET:200 Returns all of the \n\t comments for a given article', () => {
+      it('/:article_id/comments GET:200 Returns all of the comments for a given article', () => {
         return request(app)
           .get('/api/articles/1/comments')
           .expect(200)
@@ -156,7 +156,7 @@ describe('app', () => {
             expect(comments[0]).to.have.keys('comment_id', 'votes', 'created_at', 'author', 'body')
           })
       });
-      it('/:article_id/comments GET:400 Returns \'Invalid ID provided\' when \n\t passed an invalid article_id to fetch comments for', () => {
+      it('/:article_id/comments GET:400 Returns \'Invalid ID provided\' when passed an invalid article_id to fetch comments for', () => {
         return request(app)
           .get('/api/articles/The_Thiefs_Journal/comments')
           .expect(400)
@@ -164,7 +164,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Invalid ID provided');
           });
       });
-      it('/:article_id/comments GET:404 Returns \'Article does not \n\t exist\' when passed a valid article_id not \n\t found in the database', () => {
+      it('/:article_id/comments GET:404 Returns \'Article does not exist\' when passed a valid article_id not found in the database', () => {
         return request(app)
           .get('/api/articles/9999/comments')
           .expect(404)
@@ -172,7 +172,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Article does not exist');
           });
       });
-      it('/:article_id/comments GET:200 Returns an empty array \n\t when passed an extant article\'s ID with no comments', () => {
+      it('/:article_id/comments GET:200 Returns an empty array when passed an extant article\'s ID with no comments', () => {
         return request(app)
           .get('/api/articles/2/comments')
           .expect(200)
@@ -180,7 +180,7 @@ describe('app', () => {
             expect(response.body).to.deep.equal({ comments: [] });
           });
       });
-      it('/:article_id/comments POST:201 Succesfully adds a \n\t comment and returns posted comment when passed an \n\t extant article\'s ID and req.body', () => {
+      it('/:article_id/comments POST:201 Succesfully adds a comment and returns posted comment when passed an extant article\'s ID and req.body', () => {
         return request(app)
           .post('/api/articles/2/comments')
           .send({ username: 'butter_bridge', body: 'Test, test test test!' })
@@ -197,7 +197,7 @@ describe('app', () => {
               });
           });
       });
-      it('/:article_id/comments POST:400 Returns \'No username \n\t provided\' when username property not passed in sent object', () => {
+      it('/:article_id/comments POST:400 Returns \'No username provided\' when username property not passed in sent object', () => {
         return request(app)
           .post('/api/articles/2/comments')
           .send({ body: 'Shouldn\'t be added!' })
@@ -206,7 +206,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('No username provided')
           });
       });
-      it('/:article_id/comments POST:400 Returns \'No text \n\t provided\' when body property not passed in sent object', () => {
+      it('/:article_id/comments POST:400 Returns \'No text provided\' when body property not passed in sent object', () => {
         return request(app)
           .post('/api/articles/2/comments')
           .send({ username: 'Shouldn\'t be added!' })
@@ -215,7 +215,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('No text provided')
           });
       });
-      it('/:article_id/comments POST:400 Returns \'Invalid properties \n\t provided\' when properties other than body/username are \n\t passed in sent object', () => {
+      it('/:article_id/comments POST:400 Returns \'Invalid properties provided\' when properties other than body/username are passed in sent object', () => {
         return request(app)
           .post('/api/articles/2/comments')
           .send({ username: 'butter_bridge', body: 'Shouldn\'t be added!', votes: 25 })
@@ -224,7 +224,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Invalid properties provided')
           });
       });
-      it('/:article_id/comments POST:404 Returns \'Incorrect username \n\t provided\' when an incorrect username is \n\t passed in sent object', () => {
+      it('/:article_id/comments POST:404 Returns \'Incorrect username provided\' when an incorrect username is passed in sent object', () => {
         return request(app)
           .post('/api/articles/2/comments')
           .send({ username: 'butter_ridge', body: 'Shouldn\'t be added!' })
@@ -233,7 +233,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Incorrect username provided')
           });
       });
-      it('/:article_id/comments POST:404 Returns \'Article does not \n\t exist\' when passed valid but incorrect article_id', () => {
+      it('/:article_id/comments POST:404 Returns \'Article does not exist\' when passed valid but incorrect article_id', () => {
         return request(app)
           .post('/api/articles/9999/comments')
           .send({ username: 'butter_bridge', body: 'Shouldn\'t be added!' })
@@ -258,7 +258,7 @@ describe('app', () => {
             expect(response.body.comments[0].author).to.equal('butter_bridge')
           });
       });
-      it('/articles/ GET:200 Returns all of the articles, \n\t including a comment count', () => {
+      it('/articles/ GET:200 Returns all of the articles, including a comment count', () => {
         return request(app)
           .get('/api/articles/')
           .expect(200)
@@ -286,7 +286,7 @@ describe('app', () => {
             expect(response.body.articles.length).to.equal(1);
           });
       });
-      it('/api/articles?sort_by=puppies GET:400 Responds with \n\t \'Cannot sort by ${query} - ${query} column does not exist\'', () => {
+      it('/api/articles?sort_by=puppies GET:400 Responds with \'Cannot sort by ${query} - ${query} column does not exist\'', () => {
         return request(app)
           .get('/api/articles?sort_by=puppies')
           .expect(400)
@@ -294,7 +294,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Cannot sort by puppies - puppies column does not exist');
           });
       });
-      it('/api/articles?order=puppies GET:400 Responds with \n\t \'Cannot order by ${query} - order must be asc or desc\'', () => {
+      it('/api/articles?order=puppies GET:400 Responds with \'Cannot order by ${query} - order must be asc or desc\'', () => {
         return request(app)
           .get('/api/articles?order=puppies')
           .expect(400)
@@ -302,7 +302,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Cannot order by puppies - order must be asc or desc');
           });
       });
-      it('/api/articles?author=puppies GET:404 Responds with \n\t \'Author ${query} is not in the database\'', () => {
+      it('/api/articles?author=puppies GET:404 Responds with \'Author ${query} is not in the database\'', () => {
         return request(app)
           .get('/api/articles?author=puppies')
           .expect(404)
@@ -310,7 +310,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Author puppies is not in the database');
           });
       });
-      it('/api/articles?topic=puppies GET:404 Responds with \n\t \'Topic ${query} is not in the database\'', () => {
+      it('/api/articles?topic=puppies GET:404 Responds with \'Topic ${query} is not in the database\'', () => {
         return request(app)
           .get('/api/articles?topic=puppies')
           .expect(404)
@@ -329,7 +329,7 @@ describe('app', () => {
       })
     });
     describe('/comments', () => {
-      it('/:comment_id PATCH:200 Successfully adds passed \n\t number of votes to \'votes\' property and returns the \n\t modified comment', () => {
+      it('/:comment_id PATCH:200 Successfully adds passed number of votes to \'votes\' property and returns the modified comment', () => {
         return request(app)
           .patch('/api/comments/1')
           .send({ inc_votes: 404 })
@@ -342,7 +342,7 @@ describe('app', () => {
             expect(comment.votes).to.equal(420);
           });
       });
-      it('/:comment_id PATCH:404 Returns \'Comment does not exist\' \n\t when passed incorrect comment_id', () => {
+      it('/:comment_id PATCH:404 Returns \'Comment does not exist\' when passed incorrect comment_id', () => {
         return request(app)
           .patch('/api/comments/9999')
           .send({ inc_votes: 404 })
@@ -351,7 +351,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Comment does not exist');
           });
       });
-      it('/:comment_id PATCH:400 Returns \'Invalid comment ID\' \n\t when passed invalid comment_id', () => {
+      it('/:comment_id PATCH:400 Returns \'Invalid comment ID\' when passed invalid comment_id', () => {
         return request(app)
           .patch('/api/comments/The_Thiefs_Journal')
           .send({ inc_votes: 404 })
@@ -360,7 +360,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Invalid ID provided');
           });
       });
-      it('/:comment_id PATCH:400 Returns \'Invalid properties \n\t in request\' when sent property other than inc_votes', () => {
+      it('/:comment_id PATCH:400 Returns \'Invalid properties in request\' when sent property other than inc_votes', () => {
         return request(app)
           .patch('/api/comments/1')
           .send({ name: 'Jordan' })
@@ -369,7 +369,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Invalid properties in request');
           });
       });
-      it('/:comment_id PATCH:400 Responds with \'Invalid properties in \n\t request\' when passed invalid properties', () => {
+      it('/:comment_id PATCH:400 Responds with \'Invalid properties in request\' when passed invalid properties', () => {
         return request(app)
           .patch('/api/comments/1')
           .send({ inc_votes: 408, name: 'Jordan' })
@@ -378,7 +378,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Invalid properties in request')
           });
       });
-      it('/:comment_id PATCH:400 Responds with \'Invalid number \n\t of votes to add\' when passed inc_votes datatype != number', () => {
+      it('/:comment_id PATCH:400 Responds with \'Invalid number of votes to add\' when passed inc_votes datatype != number', () => {
         return request(app)
           .patch('/api/comments/1')
           .send({ inc_votes: 'Jordan' })
@@ -387,7 +387,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Invalid number of votes to add')
           })
       })
-      it('/:comment_id DELETE:200 Should delete the given comment \n\t  and respond with no content.', () => {
+      it('/:comment_id DELETE:200 Should delete the given comment and respond with no content.', () => {
         return request(app)
           .delete('/api/comments/1')
           .expect(204)
@@ -400,7 +400,7 @@ describe('app', () => {
               })
           });
       });
-      it('/:comment_id PATCH:404 Returns \'Comment does not exist\' \n\t when passed incorrect comment_id', () => {
+      it('/:comment_id PATCH:404 Returns \'Comment does not exist\' when passed incorrect comment_id', () => {
         return request(app)
           .delete('/api/comments/9999')
           .expect(404)
@@ -408,7 +408,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Comment does not exist');
           });
       });
-      it('/:comment_id PATCH:400 Returns \'Invalid comment ID\' \n\t when passed invalid comment_id', () => {
+      it('/:comment_id PATCH:400 Returns \'Invalid comment ID\' when passed invalid comment_id', () => {
         return request(app)
           .delete('/api/comments/The_Thiefs_Journal')
           .expect(400)
@@ -458,7 +458,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal('Method Not Found');
           });
       });
-      it('/:comment_id PATCH:200 Returns unchanged comment \n\t when passed no inc_votes on request body', () => {
+      it('/:comment_id PATCH:200 Returns unchanged comment when passed no inc_votes on request body', () => {
         return request(app)
           .patch('/api/comments/1')
           .send({})
@@ -467,7 +467,7 @@ describe('app', () => {
             expect(response.body.comment.votes).to.equal(16);
           })
       });
-      it('/:comment_id PATCH:200 Returns unchanged comment \n\t when passed no inc_votes on request body', () => {
+      it('/:comment_id PATCH:200 Returns unchanged comment when passed no inc_votes on request body', () => {
         return request(app)
           .put('/api/comments/1')
           .send({ user: 'butter_bridge' })
@@ -476,7 +476,7 @@ describe('app', () => {
             expect(response.body.msg).to.equal("Method Not Found");
           });
       });
-      it('/:comment_id GET:200 Returns comments \n\t sorted by votes', () => {
+      it('/:comment_id GET:200 Returns comments sorted by votes', () => {
         return request(app)
           .get('/api/comments?sort_by=votes')
           .expect(200)
