298e53107e60d67ea0f2ca866270d6dfbea7cb0e
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-01-09 09:10:27 +0000

    Moved error handling logic into discrete functions in utils file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routers/apiRouter.js')
+const { errorHandler } = require('./db/utils/utils.js')
 
 
 app.use(express.json());
@@ -13,26 +14,7 @@ app.all('/*', (req, res, next) => {
 
 
 app.use((err, req, res, next) => {
-  if (!err.status) {
-    next(err)
-  }
-  res.status(err.status).send({ msg: err.msg });
-})
-
-app.use((err, req, res, next) => {
-  const psqlErrors = {
-    "22P02": "Invalid ID provided",
-    "23503": {
-      "author": "Incorrect username provided",
-      "article_id": "Article does not exist"
-    },
-    "42703": `Cannot sort by ${req.query.sort_by} - ${req.query.sort_by} column does not exist`
-  };
-  if (err.code === "23503") {
-    res.status(404).send({ msg: psqlErrors[err.code][err.problem] })
-  } else {
-    res.status(400).send({ msg: psqlErrors[err.code] })
-  }
+  errorHandler(err, req, res, next)
 })
 
 module.exports = app;
--- db/utils/utils.js
+++ db/utils/utils.js
@@ -36,3 +36,27 @@ exports.formatComments = (comments, articleRef) => {
   })
   return editedComments;
 };
+
+exports.errorHandler = (err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    complexErrorHandler(err, req, res, next)
+  }
+}
+
+const complexErrorHandler = (err, req, res, next) => {
+  const psqlErrors = {
+    "22P02": "Invalid ID provided",
+    "23503": {
+      "author": "Incorrect username provided",
+      "article_id": "Article does not exist"
+    },
+    "42703": `Cannot sort by ${req.query.sort_by} - ${req.query.sort_by} column does not exist`
+  };
+  if (err.code === "23503") {
+    res.status(404).send({ msg: psqlErrors[err.code][err.problem] })
+  } else {
+    res.status(400).send({ msg: psqlErrors[err.code] })
+  }
+}
