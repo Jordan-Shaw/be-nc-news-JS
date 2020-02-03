4959653715090e7d494a8764c9ef76dc0ddb1e62
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-02-03 08:49:40 +0000

    remove practice files and change profile picture links

--- practice.sql
+++
@@ -1,9 +0,0 @@
-\c nc_news_test
-
-SELECT articles.*, COUNT(comment_id) AS comment_count FROM articles
-LEFT JOIN comments ON articles.article_id=comments.article_id
-WHERE articles.article_id=1
-GROUP BY articles.article_id
-;
--- JOIN comments ON articles.article_id = comments.article_id;
-  
